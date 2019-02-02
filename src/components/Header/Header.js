dce0e1864fdce4b61176d58e8be115a9db3c64ab
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-02 18:29:02 +0600

    refactor styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Header.css';
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 const Header = () => {
     return (
         <div className="Main-nav">
             <div className="Logo">Quotes Central</div>
-            <NavLink to="/">Quotes</NavLink>
-            <NavLink to="/add">Submit new quote</NavLink>
+            <div className="Menu">
+                <NavLink to="/">Quotes</NavLink>
+                <NavLink to="/add">Submit new quote</NavLink>
+            </div>
         </div>
 
     )
