df7c86ed4989ce593ab5062b6da77910ab956d32
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-02 11:37:43 +0600

    add Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <BrowserRouter>
+              <Switch>
+                  <Route path="/" exact component={QuoteBuilder} />
+                  <Route path="/quote/add" component={QuoteAdd}/>
+                  {/*<Route path="/quote/:id" component={FullQuote}/>*/}
+                  {/*<Route path="/quote/:id/edit" component={PostEdit}/>*/}
+              </Switch>
+          </BrowserRouter>
       </div>
     );
   }
