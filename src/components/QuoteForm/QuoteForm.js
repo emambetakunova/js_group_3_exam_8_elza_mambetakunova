736e3ab48f81bd082315978d2f1fbc5bec722a26
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-02 11:37:11 +0600

    add styles fo QuoteForm

---
+++ src/components/QuoteForm/QuoteForm.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {CATEGORIES} from "../../constans";
+
+class QuoteForm extends Component {
+
+    state = {
+        quoteText: '',
+        category: Object.keys(CATEGORIES)[0],
+        author: ''
+    };
+
+    valueChanged = event => {
+        const {name, value} = event.target;
+
+        this.setState({[name]: value})
+    };
+
+    submitHandler = event => {
+        event.preventDefault();
+        this.props.onSubmit({...this.state});
+    };
+
+    render() {
+        return (
+            <form className="QuoteForm" onSubmit={this.submitHandler}>
+                <h3>Submit new quote</h3>
+                <p>Category: </p>
+                <select name="category" onChange={this.valueChanged} value={this.state.category}>
+                    {Object.keys(CATEGORIES).map(categoryId => (
+                        <option key={categoryId} value={categoryId}>{CATEGORIES[categoryId]}</option>
+                    ))}
+                </select>
+                <p>Author: </p>
+                <input type="text" name="author" placeholder="Author" value={this.state.author} onChange={this.valueChanged}/>
+                <p>Quote text: </p>
+                <textarea name="quoteText" placeholder="Quote text" value={this.state.description} onChange={this.valueChanged}/>
+                <button type="submit">Save</button>
+            </form>
+        );
+    }
+}
+
+export default QuoteForm;
