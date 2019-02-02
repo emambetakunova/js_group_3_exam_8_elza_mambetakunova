0c3b584121ca6396a560cf721f69aa0d188570df
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-02 18:28:37 +0600

    add title for author

--- src/components/CardQuote/CardQuote.js
+++ src/components/CardQuote/CardQuote.js
@@ -8,8 +8,8 @@ class CardQuote extends Component {
 
         return (
             <div className="CardQuote">
-                <p>{this.props.author}</p>
                 <p>{this.props.quoteText}</p>
+                <p><strong>Author: </strong> {this.props.author}</p>
                 <button onClick={this.props.edit} className="Edit">Edit</button>
                 <button onClick={this.props.delete} className="Delete">Delete</button>
             </div>
