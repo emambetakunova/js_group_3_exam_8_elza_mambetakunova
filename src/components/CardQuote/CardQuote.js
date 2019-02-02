34c9696e91c36b3bb232584a5f966f4ab893bea5
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-02 16:26:32 +0600

    add styles for component

--- src/components/CardQuote/CardQuote.js
+++ src/components/CardQuote/CardQuote.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+import './CardQuote.css'
 
 class CardQuote extends Component {
 
@@ -9,8 +10,8 @@ class CardQuote extends Component {
             <div className="CardQuote">
                 <p>{this.props.author}</p>
                 <p>{this.props.quoteText}</p>
-                <button onClick={this.props.edit}>Edit</button>
-                <button onClick={this.props.delete}>Delete</button>
+                <button onClick={this.props.edit} className="Edit">Edit</button>
+                <button onClick={this.props.delete} className="Delete">Delete</button>
             </div>
         );
     }
